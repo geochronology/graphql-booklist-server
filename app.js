478725a2edc9ccf0e52adce4dab0b981c16f21a2
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-30 19:47:09 -0800

    added express boilerplate

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(5000, () => {
+  console.log('server is live on port 5000')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
