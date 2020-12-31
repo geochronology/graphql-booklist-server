89f4e2699619d49b89ce4f1be08ed95a52496e39
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-30 19:53:18 -0800

    added graphql boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
 const app = express()
+
+// add graphql express route handler
+app.use('/graphql', graphqlHTTP({
+  // options
+}))
 
 app.listen(5000, () => {
   console.log('server is live on port 5000')
